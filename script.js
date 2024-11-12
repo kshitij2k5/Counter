897c86fe65e13013617e48a9cae1db5712904f69
kshitij2k5 <kshitij2k5@gmail.com>
2024-11-13 00:04:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let number=0;
+function add(){
+        number++;
+        document.getElementById("Number").innerHTML=number;
+}
+function subtract(){
+    number--;
+    document.getElementById("Number").innerHTML=number;
+}
